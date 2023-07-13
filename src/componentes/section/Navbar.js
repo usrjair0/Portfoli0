250c837de4a6f125208e0b841911ce7d2bc1439b
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-07-13 19:08:19 -0300

    aula1commit

---
+++ src/componentes/section/Navbar.js
@@ -0,0 +1,8 @@
+function Navbar (){
+    return(
+        <div>
+            <h1>Aqui ficará meu Navbar</h1>
+        </div>
+    )
+}
+export default Navbar
---
+++ src/componentes/section/Projects.js
@@ -0,0 +1,8 @@
+function Projects (){
+    return (
+        <div> 
+            <h1> Aqui ficarão meus projetos</h1>
+        </div>
+    )
+}
+export default Projects;
