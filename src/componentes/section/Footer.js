15dd2aba0b6938644c25e980a59c8c67906420a5
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-07-14 20:46:54 -0300

    add cahnges aula 9

--- src/componentes/section/Footer.js
+++ src/componentes/section/Footer.js
@@ -1,7 +1,20 @@
+import styles from './Footer.module.css'
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa6";
+
 function Footer (){
     return (
-        <div> 
-            <h1> Aqui ficara o meu Footer</h1>
+        <div className={styles.footer}> 
+            <ul>
+                <li><a href='https://www.instagram.com/jairoo.jrr/'><FaInstagram size={29}/></a></li>
+                <li><a href='https://github.com/usrjair0'><FaGithub size={29}/></a></li>
+                <li><a href='https://www.linkedin.com/in/jairosilva1a/'><FaLinkedin size={29}/></a></li>
+            </ul>
+            <p>
+                juniorsilvasobrinho1999@gmail.com
+            </p>
+            <p>
+                Jairo Silva @ 2023
+            </p>
         </div>
     )
 }
