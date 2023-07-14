3215454b0cb350ac86d074a043ef820eac4a74a2
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-07-13 21:20:30 -0300

    aula 03 add some changes

--- src/componentes/section/Navbar.js
+++ src/componentes/section/Navbar.js
@@ -1,7 +1,19 @@
+import styles from './Navbar.module.css';
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa6";
+
 function Navbar (){
     return(
-        <div>
-            <h1>Aqui ficará meu Navbar</h1>
+        <div className={styles.navbar}>
+            <ul>
+                <li>Apresentação</li>
+                <li>Habilidades</li>
+                <li>Projetos</li>
+            </ul>
+            <ul>
+                <li><FaInstagram size={40}/></li>
+                <li><FaGithub size={40}/></li>
+                <li><FaLinkedin size={40}/></li>
+            </ul>
         </div>
     )
 }
