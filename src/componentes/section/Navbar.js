5469539f4fcb79c0a83bc6d33b2279b76297fdb5
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-16 23:06:39 -0300

    navbar css changes

--- src/componentes/section/Navbar.js
+++ src/componentes/section/Navbar.js
@@ -2,8 +2,8 @@ import styles from './Navbar.module.css';
 import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa6";
 import Nav from 'react-bootstrap/Nav' //importanto do react bootstrap
 
-function Navbar (){
-    return(
+function Navbar() {
+    return (
         <div className={styles.navbar}>
             <ul className={styles.cont}>
                 <li><Nav.Link href='#Presentation'>Apresentação</Nav.Link></li>
@@ -11,9 +11,9 @@ function Navbar (){
                 <li><Nav.Link href='#Projects'>Projetos</Nav.Link></li>
             </ul>
             <ul>
-                <li><a href='https://www.instagram.com/jairoo.jrr/'><FaInstagram size={40}/></a></li>
-                <li><a href='https://github.com/usrjair0'><FaGithub size={40}/></a></li>
-                <li><a href='https://www.linkedin.com/in/jairosilva1a/'><FaLinkedin size={40}/></a></li>
+                <li><a href='https://www.instagram.com/jairoo.jrr/'><FaInstagram size="2.5em" /></a></li>
+                <li><a href='https://github.com/usrjair0'><FaGithub size="2.5em" /></a></li>
+                <li><a href='https://www.linkedin.com/in/jairosilva1a/' ><FaLinkedin size="2.5em" /></a></li>
             </ul>
         </div>
     )
