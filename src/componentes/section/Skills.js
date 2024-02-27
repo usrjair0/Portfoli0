7b2ccff301e9e5226196fd8181d2726431209d20
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2024-02-27 19:31:30 -0300

    images correção firefox

--- src/componentes/section/Skills.js
+++ src/componentes/section/Skills.js
@@ -1,4 +1,9 @@
 import styles from './Skills.module.css';
+import css from '../images/icons/css.png';
+import html from '../images/icons/html.png';
+import git from '../images/icons/git.png';
+import javascript from '../images/icons/javascript.png';
+import react from '../images/icons/react.png';
 
 function Skills() {
     var corh1 = '#292424';
@@ -7,11 +12,11 @@ function Skills() {
             <h1 style={{ color: corh1 }}>Minhas Habilidades</h1>
             <p style={{ color: corh1 }}>Conheça um pouco sobre minhas principais habilidades.</p>
             <div className={styles.shields} >
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" alt='html5' />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg" alt='CSS3' />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt='javascript' />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt='react' />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg" alt='git' />
+                <img src={html} alt='html5' />
+                <img src={css} alt='CSS3' />
+                <img src={javascript} alt='javascript' />
+                <img src={react} alt='react' />
+                <img src={git} alt='git' />
             </div>
         </div>
     )
